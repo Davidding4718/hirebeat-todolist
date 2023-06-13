92db4264f3737b7ef67e4d4bc358b5873d03f1dc
Davidding4718 <109874435+Davidding4718@users.noreply.github.com>
2023-06-12 20:49:10 -0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import LoginForm from './LoginForm';  // Assuming LoginForm.js is in the same directory
+import './amplify-config';  // Load the Amplify configuration
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LoginForm />
       </header>
     </div>
   );
